fe7a5d8766d0eeb1a70d765cae6d34979648146b
Artem Chernyaev <askanio234@gmail.com>
2017-05-08 14:29:11 +0300

    Add basic server script to handle requests

--- server.py
+++ server.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request, redirect, url_for
+from db_schema import db_session, Posts
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    if request.method == 'GET':
+        return render_template('form.html', show_button=True,
+                                header="", signature="", body="")
+    if request.method == 'POST':
+        post_id = request.form['header'] + str(datetime.date.today())  
+        post_to_add = Posts(post_url=post_id,
+                            post_header=request.form['header'],
+                            post_signature=request.form['signature'],
+                            post_body=request.form['body'])
+        db_session.add(post_to_add)
+        db_session.commit()
+        return redirect(url_for('posts_by_id', post_id=post_id))
+
+
+@app.route('/posts/<post_id>')
+def posts_by_id(post_id):
+    post_to_show = db_session.query(Posts).filter(
+                                        Posts.post_url == post_id
+                                        ).first()
+    return render_template('form.html', show_button=False,
+                            header=post_to_show.post_header,
+                            signature=post_to_show.post_signature,
+                            body=post_to_show.post_body)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
