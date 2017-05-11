90be8fcc9b6aa0b8d1a4cb8949c0bac9cc3fbfbb
Artem Chernyaev <askanio234@gmail.com>
2017-05-11 21:26:02 +0300

    Add handler and customized template for the event when post is not found in db

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from sqlalchemy import exc
 from db_schema import db_session, Posts
 
@@ -35,12 +35,20 @@ def posts_by_id(post_id):
     post_to_show = db_session.query(Posts).filter(
                                         Posts.post_url == post_id
                                         ).first()
-    return render_template('form.html', show_button=False,
-                            header=post_to_show.post_header,
-                            signature=post_to_show.post_signature,
-                            body=post_to_show.post_body)
+    if post_to_show is not None:
+        return render_template('form.html', show_button=False,
+                                header=post_to_show.post_header,
+                                signature=post_to_show.post_signature,
+                                body=post_to_show.post_body)
+    else:
+        abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0' port=port)
